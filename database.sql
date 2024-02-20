b6094d133f5c818d21d26685182673e2f618d7ce
Quovadis1212 <david@stoeckli.site>
2024-02-20 19:32:03 +0100

    fixes to database.sql

--- database.sql
+++ database.sql
@@ -8,28 +8,30 @@ USE RecipeCache;
 
 -- Tabelle für Rezepte erstellen
 CREATE TABLE recipes (
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(255) NOT NULL,
-Kochzeit VARCHAR(100),
-is_vegetarian BOOLEAN,
-blacklist BOOLEAN DEFAULT FALSE,
-url VARCHAR(255) UNIQUE NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    cooking_time VARCHAR(100),
+    is_vegetarian BOOLEAN,
+    blacklist BOOLEAN DEFAULT FALSE,
+    url VARCHAR(255) UNIQUE NOT NULL
 );
+
 
 -- Tabelle für Zutaten erstellen
 CREATE TABLE ingredients (
-id INT AUTO_INCREMENT PRIMARY KEY,
-recipe_id INT,
-Zutat VARCHAR(255) NOT NULL,
-Menge VARCHAR(100),
-FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT,
+    ingredient VARCHAR(255) NOT NULL,
+    amount VARCHAR(100),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
 );
+
 
 -- Tabelle für Zubereitungsschritte erstellen
 CREATE TABLE preparation_steps (
-id INT AUTO_INCREMENT PRIMARY KEY,
-recipe_id INT,
-Schrittnummer INT,
-Schrittbeschreibung TEXT,
-FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT,
+    step_number INT,
+    step_description TEXT,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
 );
